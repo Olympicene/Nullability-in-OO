3aa41285ff126ab0692878ad1ed867b59957214d
Kevin <kevinjcord@gmail.com>
2023-11-06 16:41:34 -0600

    first commit

---
+++ interpreter.ml
@@ -0,0 +1,199 @@
+open List
+
+(* syntax *)
+type ident = string
+
+type typ = IntTy | ClassTy of ident
+type exp = Num of int | Add of exp * exp | Mul of exp * exp | Var of ident
+         | GetField of exp * ident
+
+type cmd = Assign of ident * exp | Seq of cmd * cmd | Skip
+         | New of ident * ident * exp list
+         | Invoke of ident * exp * ident * exp list | Return of exp
+
+
+type mdecl = { ret : typ; mname : ident; params : (typ * ident) list; body : cmd }
+
+type cdecl = { cname : ident; super : ident; fields : (typ * ident) list; methods : mdecl list }
+
+(* contexts *)
+type ty_entry = Ty of typ
+              | Class of cdecl
+
+type context = ident -> ty_entry option
+let empty_context = fun x -> None
+let context_lookup (gamma : context) (x : ident) : ty_entry option = gamma x
+let context_update (gamma : context) (x : ident) (e : ty_entry) = fun y -> if y = x then Some e else gamma y
+
+let lookup_var (gamma : context) (x : ident) : typ option =
+  match context_lookup gamma x with Some (Ty t) -> Some t | _ -> None
+let lookup_class (gamma : context) (x : ident) : cdecl option =
+  match context_lookup gamma x with Some (Class cd) -> Some cd | _ -> None
+let update_var (gamma : context) (x : ident) (t : typ) : context = context_update gamma x (Ty t)
+let update_class (gamma : context) (x : ident) (c : cdecl) : context = context_update gamma x (Class c)
+
+(* field and method lookup *)
+let rec fields (ct : context) (c : ident) : (typ * ident) list =
+  if c = "Object" then [] else
+    match lookup_class ct c with
+    | Some cd -> fields ct cd.super @ cd.fields
+    | _ -> []
+
+let types_of_params (params : (typ * ident) list) : typ list =
+  List.map fst params
+
+let names_of_params (params : (typ * ident) list) : ident list =
+    List.map snd params
+  
+let field_type_aux (l : (typ * ident) list) (f : ident) : typ option =
+  match List.find_opt (fun (_, n) -> n = f) l with
+  | Some (t, _) -> Some t
+  | _ -> None
+
+let field_type (ct : context) (c : ident) (f : ident) : typ option =
+  field_type_aux (rev (fields ct c)) f
+
+let rec methods (ct : context) (c : ident) : mdecl list =
+  if c = "Object" then [] else
+    match lookup_class ct c with
+    | Some cd -> methods ct cd.super @ cd.methods
+    | _ -> []
+
+let lookup_method_aux (l : mdecl list) (m : ident) : mdecl option =
+  find_opt (fun d -> d.mname = m) l
+
+let lookup_method (ct : context) (c : ident) (m : ident) : mdecl option =
+  lookup_method_aux (rev (methods ct c)) m
+
+(* semantics *)
+type reference = int
+type value = IntVal of int | RefVal of reference
+
+(* environment implementation *)
+type env = ident -> value option
+let empty_env = fun x -> None
+let lookup (r : env) (x : ident) : value option = r x
+let update (r : env) (x : ident) (e : value) : env = fun y -> if y = x then Some e else r y
+
+let rec add_args (r : env) (li : ident list) (lv : value list) : env =
+  match li, lv with
+  | i :: irest, v :: vrest -> add_args (update r i v) irest vrest
+  | _, _ -> r
+(* end environment implementation *)
+
+(* store implementation *)
+type obj = ObjVal of ident (* class name *) * env (* field values *)
+type store = (reference -> obj option) * int
+let empty_store : store = ((fun x -> None), 0)
+let store_lookup (s : store) (x : reference) : obj option = (fst s) x
+let store_update (s : store) (x : reference) (o : obj) : store = let (r, n) = s in ((fun y -> if y = x then Some o else r y), n)
+let alloc (s : store) (o : obj) : store * reference = let (r, n) = s in (((fun y -> if y = n then Some o else r y), n + 1), n)
+(* end store implementation *)
+
+(* Semantics *)
+let rec eval_exp (e : exp) (r : env) (s : store) : value option =
+  match e with
+  | Var x -> lookup r x
+  | Num i -> Some (IntVal i)
+  | Add (e1, e2) -> (match eval_exp e1 r s, eval_exp e2 r s with
+                     | Some (IntVal i1), Some (IntVal i2) -> Some (IntVal (i1 + i2))
+                     | _, _ -> None)
+  | Mul (e1, e2) -> (match eval_exp e1 r s, eval_exp e2 r s with
+                     | Some (IntVal i1), Some (IntVal i2) -> Some (IntVal (i1 * i2))
+                     | _, _ -> None)
+  | GetField (e, f) -> (match eval_exp e r s with
+                        | Some (RefVal p) -> (match store_lookup s p with
+                                              | Some (ObjVal (_, fs)) -> lookup fs f
+                                              | _ -> None)
+                        | _ -> None)
+
+let rec eval_exps (es : exp list) (r : env) (s : store) : value list option =
+  match es with
+  | [] -> Some []
+  | e :: rest -> (match eval_exp e r s, eval_exps rest r s with
+                  | Some v, Some vs -> Some (v :: vs)
+                  | _, _ -> None)
+
+type stack = (env * ident) list
+
+type config = cmd * stack * env * store
+
+let rec step_cmd (gamma : context) (con : config) : config option =
+  let (c, k, r, s) = con in
+  match c with
+  | Assign (x, e) -> (match eval_exp e r s with
+                      | Some v -> Some (Skip, k, update r x v, s)
+                      | None -> None)
+  | Seq (Skip, c2) -> Some (c2, k, r, s)
+  | Seq (c1, c2) -> (match step_cmd gamma (c1, k, r, s) with
+                     | Some (c1', k', r', s') -> Some (Seq (c1', c2), k', r', s')
+                     | None -> None)
+  | Skip -> None
+  | New (x, c, es) -> (match eval_exps es r s with
+                       | Some vs -> let (s', p) = alloc s (ObjVal (c, add_args empty_env (names_of_params (fields gamma c)) vs)) in
+                                      Some (Skip, k, update r x (RefVal p), s')
+                       | _ -> None)
+  | Invoke (x, e, m, es) -> (match eval_exp e r s, eval_exps es r s with
+                             | Some (RefVal p), Some vs ->
+                                 (match store_lookup s p with
+                                  | Some (ObjVal (c, _)) ->
+                                      (match lookup_method gamma c m with
+                                       | Some md -> Some (md.body, (r, x) :: k, update (add_args r (names_of_params md.params) vs) "this" (RefVal p), s)
+                                       | _ -> None)
+                                  | _ -> None)
+                             | _, _ -> None)
+  | Return e -> (match eval_exp e r s, k with
+                 | Some v, (r', x) :: k' -> Some (Skip, k', update r' x v, s)
+                 | _, _ -> None)
+
+let rec run_config gamma (con : config) : config =
+  match step_cmd gamma con with
+  | Some con' -> run_config gamma con'
+  | None -> con
+
+let run_prog gamma (c : cmd) =
+  run_config gamma (c, [], empty_env, empty_store)
+
+(* test cases *)
+let ct0 = context_update (context_update empty_context
+    "Shape" (Class {cname = "Shape"; super = "Object"; fields = [(IntTy, "id")];
+          methods = [{ret = IntTy; mname = "area"; params = []; body = Return (Num 0)}]}))
+    "Square" (Class {cname = "Square"; super = "Shape"; fields = [(IntTy, "side")];
+               methods = [{ret = IntTy; mname = "area"; params = [];
+                    body = Seq (Assign ("x", GetField (Var "this", "side")),
+                       Return (Add (Var "x", Var "x")))}]})
+
+let fields0 = update empty_env "id" (IntVal 2)
+let (store0, ref0) = alloc empty_store (ObjVal ("Shape", fields0))
+let env0 = update empty_env "s" (RefVal ref0)
+
+let exp2 : exp = GetField (Var "s", "id")
+  
+let cmd3 : cmd =
+  Seq (New ("s", "Square", [Num 0; Num 2]),
+       (* s = new Square(0, 2); *)
+       Assign ("y", Add (GetField (Var "s", "side"), Num 1)))
+       (* y = s.side + 1; *)
+  
+let cmd4 : cmd =
+  Seq (New ("s", "Shape", [Num 2]),
+       (* s = new Shape(2); *)
+       Invoke ("x", Var "s", "area", []))
+       (* x = s.area(); *)
+  
+let cmd5 : cmd =
+  Seq (New ("s", "Square", [Num 0; Num 2]),
+       (* s = new Square(0, 2); *)
+  Seq (Assign ("y", Add (GetField (Var "s", "side"), Num 1)),
+       (* y = s.side + 1; *)
+       Invoke ("x", Var "s", "area", [])))
+       (* x = s.area(); *)
+
+(* run the tests *)
+let field_test1 = eval_exp exp2 env0 store0
+  
+let new_test1 = run_prog ct0 cmd3
+  
+let invoke_test1 = run_prog ct0 cmd4
+  
+let invoke_test2 = run_prog ct0 cmd5
---
+++ typechecker.ml
@@ -0,0 +1,172 @@
+(* open List *) (* Fuck off, open statements. *) (* Now that I've deleted you (and fixed the errors), the lsp properly describes lists as lists, not t.*)
+
+(* syntax *)
+type ident = string
+
+type typ = IntTy | ClassTy of ident (* The kinds of types a variable can have. *)
+type exp = Num of int | Add of exp * exp | Mul of exp * exp | Var of ident
+         | GetField of exp * ident
+
+type cmd = Assign of ident * exp | Seq of cmd * cmd | Skip
+         | New of ident * ident * exp list
+         | Invoke of ident * exp * ident * exp list | Return of exp
+
+
+type mdecl = { ret : typ; mname : ident; params : (typ * ident) list; body : cmd }
+
+type cdecl = { cname : ident; super : ident; fields : (typ * ident) list; methods : mdecl list }
+
+(* contexts *)
+type ty_entry = Ty of typ (* Entries in the type context can either be the types of variables, or the definitions of classes. *)
+              | Class of cdecl
+
+type context = ident -> ty_entry option
+let empty_context = fun x -> None
+let lookup (gamma : context) (x : ident) : ty_entry option = gamma x
+let update (gamma : context) (x : ident) (e : ty_entry) = fun y -> if y = x then Some e else gamma y
+
+let lookup_var (gamma : context) (x : ident) : typ option =
+  match lookup gamma x with Some (Ty t) -> Some t | _ -> None
+let lookup_class (gamma : context) (x : ident) : cdecl option =
+  match lookup gamma x with Some (Class cd) -> Some cd | _ -> None
+let update_var (gamma : context) (x : ident) (t : typ) : context = update gamma x (Ty t)
+let update_class (gamma : context) (x : ident) (c : cdecl) : context = update gamma x (Class c)
+
+(* field and method lookup *)
+let rec fields (ct : context) (c : ident) : (typ * ident) list =
+  if c = "Object" then [] else
+    match lookup_class ct c with
+    | Some cd -> fields ct cd.super @ cd.fields
+    | _ -> []
+
+let types_of_params (params : (typ * ident) list) : typ list =
+  List.map fst params
+
+let field_type_aux (l : (typ * ident) list) (f : ident) : typ option =
+  match List.find_opt (fun (_, n) -> n = f) l with
+  | Some (t, _) -> Some t
+  | _ -> None
+
+let field_type (ct : context) (c : ident) (f : ident) : typ option =
+  field_type_aux (List.rev (fields ct c)) f
+
+let rec methods (ct : context) (c : ident) : mdecl list =
+  if c = "Object" then [] else
+    match lookup_class ct c with
+    | Some cd -> methods ct cd.super @ cd.methods
+    | _ -> []
+
+let lookup_method_aux (l : mdecl list) (m : ident) : mdecl option =
+  List.find_opt (fun d -> d.mname = m) l
+
+let lookup_method (ct : context) (c : ident) (m : ident) : mdecl option =
+  lookup_method_aux (List.rev (methods ct c)) m
+
+let rec supers (ct : context) (c : ident) : ident list = (* Answer to problem 1*)
+  match lookup_class ct c with
+  | None -> []
+  | Some cd when cd.super = "Object" -> ["Object"]
+  | Some cd -> cd.super::(supers ct cd.super) ;;
+
+let subtype (ct : context) (t1 : typ) (t2 : typ) : bool = (t1 = t2) ||
+  match t1, t2 with
+  | ClassTy c1, ClassTy c2 -> List.exists ((=) c2) (supers ct c1) (* Haha! I have made this perhaps *too* concise! *)
+  | _, _ -> false
+    
+let rec type_of (gamma : context) (e : exp) : typ option =
+  match e with
+  | Num i -> Some IntTy
+  | Add (e1, e2) | Mul (e1, e2) ->
+      (match type_of gamma e1, type_of gamma e2 with
+       | Some IntTy, Some IntTy -> Some IntTy
+       | _, _ -> None)
+  | Var x -> lookup_var gamma x  
+  | GetField (obj, f) -> (*Answer to problem 2 *)
+      (match type_of gamma obj with 
+       | Some ClassTy c -> field_type gamma c f 
+       | _ -> None)
+;;
+
+let typecheck (gamma : context) (e : exp) (t : typ) : bool =
+  match type_of gamma e with
+  | Some t1 -> subtype gamma t1 t
+  | None -> false
+
+let rec typecheck_list (gamma : context) (es : exp list) (ts : typ list) : bool =
+  List.for_all2 (typecheck gamma) es ts
+  
+let rec typecheck_cmd (gamma : context) (c : cmd) : bool =
+  match c with
+  | Assign (i, e) ->
+      (match lookup_var gamma i with
+       | Some t -> typecheck gamma e t
+       | None -> false)
+  | Seq (c1, c2) -> typecheck_cmd gamma c1 && typecheck_cmd gamma c2
+  | Skip -> true
+  | New (target, c, cargs) -> (* Problem 3 solution here *) (* TODO: Fix this to care about the type of the target identifier. *)
+      (match lookup_var gamma target with
+       | Some targetTyp -> (subtype gamma (ClassTy c) targetTyp) && (fields gamma c |> types_of_params |> typecheck_list gamma cargs)
+       | _ -> false)
+  | Invoke (varname, obj, m, arglist) -> (match type_of gamma obj with (* Problem 4 solution here, though I am not a grad student. *)
+                                            | Some ClassTy c -> (match lookup_method gamma c m, lookup_var gamma varname with 
+                                                                | Some mdec, Some ty -> mdec.params |> types_of_params |> typecheck_list gamma arglist 
+                                                                  && (subtype gamma mdec.ret ty)
+                                                                | _, _ -> false)
+                                            | _ -> false )
+  (* problem 4 (grad only) *)
+  | Return e ->
+      (match lookup_var gamma "__ret" with
+       | Some t -> typecheck gamma e t
+       | None -> false)
+
+(* test cases *)  
+let ct0 = update (update empty_context
+    "Shape" (Class {cname = "Shape"; super = "Object"; fields = [(IntTy, "id")];
+          methods = [{ret = IntTy; mname = "area"; params = []; body = Return (Num 0)}]}))
+    "Square" (Class {cname = "Square"; super = "Shape"; fields = [(IntTy, "side")];
+               methods = [{ret = IntTy; mname = "area"; params = [];
+                    body = Seq (Assign ("x", GetField (Var "this", "side")),
+                       Return (Add (Var "x", Var "x")))}]})
+
+let gamma0 : context = update_var (update_var (update_var ct0 "s" (ClassTy "Square")) "x" IntTy) "y" IntTy
+
+let gamma1 : context = update_var (update_var (update_var ct0 "s" (ClassTy "Shape")) "x" IntTy) "y" IntTy
+
+let gamma2 : context = update_var (update_var (update_var (update_var ct0 "s2" (ClassTy "Square")) "s1" (ClassTy "Shape")) "x" IntTy) "y" IntTy
+
+
+let exp2 : exp = GetField (Var "s", "id")
+  
+let cmd3 : cmd =
+  Seq (New ("s", "Square", [Num 0; Num 2]),
+       (* s = new Square(0, 2); *)
+       Assign ("y", Add (GetField (Var "s", "side"), Num 1)))
+       (* y = s.side + 1; *)
+  
+(* for the grad student problem *)
+let cmd4 : cmd =
+  Seq (New ("s", "Shape", [Num 2]),
+       (* s = new Shape(2); *)
+       Invoke ("x", Var "s", "area", []))
+       (* x = s.area(); *)
+  
+let cmd5 : cmd =
+  Seq (New ("s", "Square", [Num 0; Num 2]),
+       (* s = new Square(0, 2); *)
+  Seq (Assign ("y", Add (GetField (Var "s", "side"), Num 1)),
+       (* y = s.side + 1; *)
+       Invoke ("x", Var "s", "area", [])))
+       (* x = s.area(); *)
+
+(* run the tests *)
+let supers_test1 = subtype ct0 (ClassTy "Square") (ClassTy "Object") (* should return true *)
+
+let supers_test2 = subtype ct0 (ClassTy "Object") (ClassTy "Square") (* should return false *)
+
+let field_test1 = (type_of gamma0 exp2 = Some IntTy) (* should return true *)
+  
+let new_test1 = typecheck_cmd gamma0 cmd3 (* should return true *)
+  
+let invoke_test1 = typecheck_cmd gamma1 cmd4 (* should return true *)
+  
+let invoke_test2 = typecheck_cmd gamma0 cmd5 (* should return true *)
